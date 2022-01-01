8367ed78961f705db827f59076c567e44b64a5e6
Sammy Guergachi <sguergachi@gmail.com>
2022-01-01 03:43:14 -0500

    config prismic

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,16 @@ module.exports = {
     title: "Personal-Site-V2",
   },
   plugins: [
+    'gatsby-plugin-image',
+    {
+      resolve: 'gatsby-source-prismic',
+      options: {
+        repositoryName: process.env.GATSBY_PRISMIC_REPO_NAME,
+        accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+        customTypesApiToken: process.env.PRISMIC_CUSTOM_TYPES_API_TOKEN,
+        linkResolver: (doc) => linkResolver(doc),
+      },
+    },
     "gatsby-plugin-sass",
     "gatsby-plugin-image",
     "gatsby-plugin-react-helmet",
