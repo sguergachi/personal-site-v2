7ae9c66f04643fad50d83541b28a397d562dabbc
Sammy Guergachi <sguergachi@gmail.com>
2022-02-20 23:08:34 -0500

    wip

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,20 +9,22 @@ const IndexPage = () => {
   return (
     <Layout pageTitle="Sammy Guergachi">
       <title>Home Page</title>
-      <div class ="text-2xl text-center"> My name is
-       <br/>
-        <span class="blue">S</span>ammy <span class="blue">G</span>uer<i class="fa-solid fa-circle"></i>ga<i class="fa-solid fa-circle"></i>chi</div>
-      <p >
-        <code>Work In Progress...</code>
-      </p>
-      <button class="bg-sky-600 hover:bg-sky-700 ...">Save changes</button>
-      <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+      <div class="text-2xl text-center">
+        {" "}
+        My name is
+        <br />
+        <span class="blue">S</span>ammy <span class="blue">G</span>uer
+        <i class="fa-solid fa-circle"></i>ga<i class="fa-solid fa-circle"></i>
+        chi
+      </div>
+
+      <div class="absolute translate-x-1/2 bottom-10 p-6 max-w-md mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
         <div class="shrink-0">
           <img class="h-12 w-12" src={logo} />
         </div>
         <div>
-          <div class="text-xl font-medium text-black">ChitChat</div>
-          <p class="text-slate-500">You have a new message!</p>
+          <div class="text-xl font-medium text-black">Website Construction in Progress</div>
+          <p class="text-slate-500">I'm working on it!</p>
         </div>
       </div>
     </Layout>
