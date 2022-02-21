a9a0121a05525cb4240aa24f6907c01cccdbe66d
Sammy Guergachi <sguergachi@gmail.com>
2022-02-20 23:35:27 -0500

    style

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,11 @@
 import * as React from "react";
 import { Link } from "gatsby";
-import { container } from "./layout.module.css";
+import {
+  container,
+  navLinks,
+  navLinkItem,
+  navLinkText,
+} from "./layout.module.css";
 import Root from "./root";
 
 const Layout = ({ pageTitle, children }) => {
@@ -8,19 +13,29 @@ const Layout = ({ pageTitle, children }) => {
     <Root>
       <div className={container}>
         <title>{pageTitle}</title>
-        <nav class="">
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
+        <nav class="text-center">
+          <ul className={navLinks}>
+            <li className={navLinkItem}>
+              <Link className={navLinkText} to="/">
+                Home
+              </Link>
             </li>
-            <li>
-              <Link to="/who">Who</Link>
+            <li className={navLinkItem}>
+              <Link className={navLinkText} to="/who">
+                Who?
+              </Link>
             </li>
           </ul>
         </nav>
         <main>
-          <h1 class="text-slate-600 text-7xl text-center">{pageTitle}</h1>
+          <div class="relative select-none -z-0">
+            <h1 class="text-slate-700  opacity-10 absolute top-20 left-1/2 transform  -translate-y-1/2 -translate-x-1/2 lg:text-8xl sm:text-6xl text-center">
+              {pageTitle}
+            </h1>
+          </div>
+          <div className="z-10 select-auto">
           {children}
+          </div>
         </main>
       </div>
     </Root>
