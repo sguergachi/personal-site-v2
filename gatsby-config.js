96ff7c3e689f6a3f10dce787cdba8c4404e4fbb4
sguergachi <sguergachi@gmail.com>
2024-04-28 09:03:41 -0400

    remove plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,45 +10,4 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "Personal-Site-V2",
   },
-  plugins: [
-    {
-      resolve: 'gatsby-source-prismic',
-      options: {
-        repositoryName: process.env.GATSBY_PRISMIC_REPO_NAME,
-        accessToken: process.env.PRISMIC_ACCESS_TOKEN,
-        customTypesApiToken: process.env.PRISMIC_CUSTOM_TYPES_API_TOKEN,
-        linkResolver: (doc) => linkResolver(doc),
-      },
-    },
-    "gatsby-plugin-sass",
-    "gatsby-plugin-image",
-    "gatsby-plugin-react-helmet",
-    {
-      resolve: "gatsby-plugin-manifest",
-      options: {
-        icon: "src/images/Icon.svg",
-        
-      },
-    },
-    "gatsby-plugin-postcss",
-    "gatsby-plugin-mdx",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "pages",
-        path: "./src/pages/",
-      },
-      __key: "pages",
-    },
-  ],
 };
